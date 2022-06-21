9939b730d72632a6a006b78615b771b69599f4ab
Thomas Selden <thomasselden42@gmail.com>
2022-06-20 21:20:24 -0400

    fixed route

---
+++ docs/precache-manifest.015792abb62686e2ef9280bcaa54af17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2bbbae38aaa254b8606a69dc179618",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "f85ccb0643276b98f9b2",
+    "url": "/react-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "de08b76ea694a6272019",
+    "url": "/react-portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "f85ccb0643276b98f9b2",
+    "url": "/react-portfolio/static/js/2.7f2747d0.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-portfolio/static/js/2.7f2747d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de08b76ea694a6272019",
+    "url": "/react-portfolio/static/js/main.67d07fc0.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
