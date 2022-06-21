b0b6771fc6550f1f091b744e8aeef596999c4d93
Thomas Selden <thomasselden42@gmail.com>
2022-06-21 16:15:51 -0400

    Updates

---
+++ precache-manifest.277157a1a3e67fdebe7188141f8158f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cedf695cfc5c67ecb2ce28d08a5346f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d8daf7aaae7f5cad01a",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "9b16688183ed346fced4",
+    "url": "/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "8d8daf7aaae7f5cad01a",
+    "url": "/static/js/2.a291a6b7.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.a291a6b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b16688183ed346fced4",
+    "url": "/static/js/main.6310df52.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
